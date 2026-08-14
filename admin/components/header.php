9a49197a4317f0a80f2b34879809090fe1686e08
Tushar <tusharpatel1093@gmail.com>
2026-08-14 11:20:35 +0530

    fixup! How to header menu active functionality

--- admin/components/header.php
+++ admin/components/header.php
@@ -50,7 +50,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_roles'] == 'Users') {
                     <span class="nav-icon"><i class="bi bi-list" aria-hidden="true"></i></span>
                     <span class="nav-text">View Rooms</span>
                 </a>
-                <a class="nav-link <?php echo $currentPage == "add_rooms.php" ? 'active' : ''; ?> " href="add-user.html">
+                <a class="nav-link <?php echo $currentPage == "Add_Rooms.php" ? 'active' : ''; ?> " href="Add_Rooms.php">
                     <span class="nav-icon"><i class="bi bi-person-plus" aria-hidden="true"></i></span>
                     <span class="nav-text">Add Rooms</span>
                 </a>
